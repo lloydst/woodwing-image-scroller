685dc66199af6ec247d26ea63711e8213c58eff5
lloydst <lloyd.stumpel@gmail.com>
2018-12-22 17:18:39 +0100

    refactored scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { FlickrService } from './services/flickr.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
+  
 })
 export class AppComponent {
   title = 'image-grid-scroller';
   data
   constructor (private service: FlickrService) {}
-  
+
   find(query){
 	  this.service.search(query).subscribe(result=>{
 		  this.data = result;
