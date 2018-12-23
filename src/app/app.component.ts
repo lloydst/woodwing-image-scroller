a5c35245540995e0acbfdc5c618317875b930655
lloydst <lloyd.stumpel@gmail.com>
2018-12-23 17:25:49 +0100

    image hover text& auto grid & more images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,30 @@ import { Component } from '@angular/core';
 import { FlickrService } from './services/flickr.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-  
+	selector: 'app-root',
+	templateUrl: './app.component.html'
+
 })
 export class AppComponent {
-  title = 'image-grid-scroller';
-  data
-  constructor (private service: FlickrService) {}
+	title = 'image-grid-scroller';
+	data;
+	photoArray;
+	constructor(private service: FlickrService) { }
 
-  find(query){
-	  this.service.search(query).subscribe(result=>{
-		  this.data = result;
-	  })
-  }
+	find(query) {
+		this.service.search(query).subscribe(result => {
+			this.data = result;
+			this.makeDataGreatAgain()
+		})
+	}
+	makeDataGreatAgain() {
+		let array = this.data.photos.photo
+		console.log(array)
+		while (array.length < 10000) {
+			array.length + this.data.photos.photo.length
+			array = array.concat(this.data.photos.photo)
+			console.log(array.length, array)
+			this.data.photos.photo = array
+		}
+	}
 }
